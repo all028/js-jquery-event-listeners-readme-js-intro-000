2960108a24772e57c3e72e3bd30e55126a32c96e
al <albliu@email.com>
2019-12-10 02:23:06 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,7 @@ function getIt(){
 
 function frameIt(){
   $("form").on("load", function() {
-    .tasty {border-color: red; border-style: solid; }
+  //  .tasty {border-color: red; border-style: solid; }
 
 
     $('img').append('<img border="tasty" src="https://s3.amazonaws.com/after-school-assets/minion-tongue.jpg" />')
